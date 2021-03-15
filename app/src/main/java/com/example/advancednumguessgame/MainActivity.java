3615e3c67503cfe502f3dd348d0ddf3fbed99a86
SnippySnappingTurtle <70359000+SnippySnappingTurtle@users.noreply.github.com>
2021-03-14 20:54:16 -0500

    constraints and some code

--- app/src/main/java/com/example/advancednumguessgame/MainActivity.java
+++ app/src/main/java/com/example/advancednumguessgame/MainActivity.java
@@ -14,7 +14,10 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private int num1, num2, num3, num4;
-    private int score;
+    private int score = 0;
+    public int counter;
+    Button startButton;
+    TextView timer;
 
     private void roll() {
         Random r = new Random();
@@ -36,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         while (num3 == num4)
             num4 = r.nextInt(9);
 
+
         Button b1 = (Button) findViewById(R.id.button1);
         b1.setText("" + num1);
 
@@ -49,16 +53,24 @@ public class MainActivity extends AppCompatActivity {
         b4.setText("" + num4);
 
 
+
     }
 
-    private void check(int average) {
-        if (average - num1 < average - num2 || average - num1 < average - num3 || average - num1 < average- num4){
+    private void check(int num1, int num2, int num3, int num4) {
+
+        double average = (num1+num2+num3+num4)/4;
+        double rem1 = Math.abs(num1-average);
+        double rem2 = Math.abs(num2-average);
+        double rem3 = Math.abs(num3-average);
+        double rem4 = Math.abs(num4-average);
+
+        if (rem1 < rem2 && rem1 < rem3 && rem1 < rem4) {
             score++;
             Toast.makeText(this, "Well done!", Toast.LENGTH_SHORT).show();
         } else {
             score--;
             Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
-        }
+            }
 
         TextView txtScore = (TextView) findViewById(R.id.txtScore);
         txtScore.setText("Score:" + score);
@@ -67,18 +79,30 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void clickbutton1(View view) {
-        int average = (num1 + num2 + num3 + num4)/4;
+    public void ClickButton1(View view) {
+        check(num1, num2, num3, num4);
+
+        roll();
+
     }
 
-    public void clickbutton2(View view) { int average = (num1 + num2 + num3 + num4)/4;
+    public void ClickButton2(View view) {
+        check(num1, num2, num3, num4);
+
+        roll();
     }
 
-    public void clickbutton3(View view) {
-        int average = (num1 + num2 + num3 + num4)/4;
+    public void ClickButton3(View view) {
+
+        check(num1, num2, num3, num4);
+
+        roll();
     }
-    public void clickbutton4(View view) {
-        int average = (num1 + num2 + num3 + num4)/4;
+    public void ClickButton4(View view) {
+
+        check(num1, num2, num3, num4);
+
+        roll();
     }
 
     @Override
